d75d6403b41c5829e2769a8f3d5acfc683bab310
안상욱 <>
2020-12-13 15:42:29 +0900

    first commit

---
+++ Alamo201213/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alamo201213
+//
+//  Created by SWAhn on 2020/12/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
